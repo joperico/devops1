1d4cc936e7c0ab91275c6e0e46fc923b37fccd76
joperico <estevezcarmona15@gmail.com>
2026-05-06 19:57:32 +0200

    Probando lint flake8 con fichero añadido corregido

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """ Docstring del módulo """
 
 
-    def suma(a: int, b: int) -> int:
+        def suma(a: int, b: int) -> int:
         """ docstring function
         # debe dar errores en linter
         """
